6da634ef120279b034562c7b3f7e50b5e01046e7
Josearmandoforerogonzalez <joseforero0310@gmail.com>
2021-07-12 21:24:04 -0500

    operadores de asigancion 12/07/2021

---
+++ main.js
@@ -0,0 +1,42 @@
+// adision
+// let numUno = parseInt(prompt("esto es una suma, ingrese un valor"))
+// let numDos = parseInt(prompt("esto es una suma, ingrese el segundo  valor"))
+// numUno+=numDos
+// alert("el resultado de tu suma es " + numUno)
+
+// sustraccion
+// bender rodriguez
+// let numUno = parseInt(prompt("esto es una resta, ingrese un valor"))
+// let numDos = parseInt(prompt("esto es una resta, ingrese el segundo  valor"))
+// numUno-=numDos
+// alert("el resultado de tu resta es " + numUno)
+
+
+// multiplicacion
+// let numUno = parseInt(prompt("esto es una multiplicacion, ingrese un valor"))
+// let numDos = parseInt(prompt("esto es una multiplicacion, ingrese el segundo  valor"))
+// numUno*=numDos
+// alert("el resultado de tu multiplicacion es " + numUno)
+
+
+// division
+// let numUno = parseInt(prompt("esto es una division, ingrese un valor"))
+// let numDos = parseInt(prompt("esto es una division, ingrese el segundo  valor"))
+// numUno/=numDos
+// alert("el resultado de tu division es " + numUno.toFixed(2))
+
+// resto
+// let numUno = parseInt(prompt("esto es resto, ingrese un valor"))
+// let numDos = parseInt(prompt("esto es resto, ingrese el segundo  valor"))
+// numUno%=numDos
+// alert("el resultado del resto es " + numUno)
+
+
+// exponente
+// let numUno = parseInt(prompt("esto es multiplo, ingrese un valor"))
+// let numDos = parseInt(prompt("esto es multiplo, ingrese el segundo  valor"))
+// numUno**=numDos
+// alert("el resultado del multiplo es " + numUno)
+
+
+
